841a7645a783b1177df7fd464884081eed755acf
Shitesh Labh <shiteshlabh@gmail.com>
2023-01-17 18:10:57 -0500

    Commit and push as per project

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+const homepage = 'https://www.demoblaze.com'
+describe('Homepage Test Cases', () => {
+  beforeEach(() => {
+    //cy.viewport(1366,768);
+    cy.visit(homepage)
+  })
+  it('TC-HOME-001:Verify that user can browse the hompage and see 9 items as the product list', () => {
+    cy.get('div.card.h-100').should('have.length', 9).wait(1000).each($el => {
+      cy.wrap($el).find('div.card-block>h4')
+      cy.wrap($el).find('div.card-block>h5').should('contain','$')
+      cy.wrap($el).find('div.card-block>p').should('have.class','card-text')
+    })
   })
 })
